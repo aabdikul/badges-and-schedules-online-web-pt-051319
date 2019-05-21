5e3b70f98348825d46d9a01cab05763add13b06e
Aida Abdikulova <abdikulova@gmail.com>
2019-05-21 19:47:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,6 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator()
+def batch_badge_creator(name)
   attendees.each { |name| puts "Hello, my name is #{name}".}
 end
